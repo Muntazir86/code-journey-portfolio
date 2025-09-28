b2f8dfed803eb02452885957625953e2788a4642
Ghulam Shabbir <ghulamshabbirmtn500@gmail.com>
2025-09-28 15:58:36 +0500

    Updates

---
+++ _next/static/chunks/38c3ff4f61949a2d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3f1c03de24252e9e.js",
+  "static/chunks/turbopack-e305bb1f9c3e773e.js"
+])
---
+++ _next/static/nNvtAxvZKfWMYvaE5PDvc/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/38c3ff4f61949a2d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/code-journey-portfolio//_next/:path+",
+        "destination": "/code-journey-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
